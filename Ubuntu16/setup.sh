77ec12365139cb188678c6dd7c819eac3439d91b
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-08-17 00:56:17 +0900

    Update setup.sh

--- Ubuntu16/setup.sh
+++ Ubuntu16/setup.sh
@@ -8,6 +8,7 @@ apt-get install git wget unzip vim curl -y
 apt-get install build-essential checkinstall -y
 apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
+pip3 install pwntools
 
 cd /opt
 wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
