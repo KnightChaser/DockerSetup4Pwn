cdb973f1feda99cb6abb08be78431100b3fcd171
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-09-03 22:12:26 +0900

    Update setup.sh

--- Ubuntu16/setup.sh
+++ Ubuntu16/setup.sh
@@ -9,20 +9,20 @@
 cd ~ 
 apt-get update
 apt-get upgrade -y
-apt-get install git wget unzip vim curl -y
+apt-get install git wget unzip vim curl gcc python3-dev -y
 
 # install and compile python 3.8, maximum version.
 apt-get install build-essential checkinstall -y
 apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
-pip3 install pwntools
 
-cd /opt
+pip3 install --upgrade pip
 wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
-tar xzf Python-3.8.1.tgz
+tar xvfz Python-3.8.1.tgz
 cd Python-3.8.1
-./configure --enable-optimizations
-make altinstall
+./configure && make && make install
+cd ..
+pip3 install pwntools
 
 # PIP installation
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
